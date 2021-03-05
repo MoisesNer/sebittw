f25d7c7b53ca2c05498ba57b72363dd037de853c
Moises Nervis <StarCommand@Moisess-iMac.local>
2021-03-05 08:54:19 -0800

    adding swiper carousel

--- subPages/subApp.js
+++ subPages/subApp.js
@@ -12,3 +12,24 @@ const btnScrollTop = document.querySelector('.btnScrollTop');
 btnScrollTop.addEventListener('click', function(){
     window.scrollTo(0,0)
 })
+
+// SWIPER
+
+var swiper = new Swiper('.swiper-container', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 20,
+    stretch: 0,
+    depth: 200,
+    modifier: 1,
+    slideShadows: true,
+  },
+  loop:true,
+  autoplay: {
+    delay: 1500,
+    disableOnInteraction: false,
+  }
+});
